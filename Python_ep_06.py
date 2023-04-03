63f887e141b814b70f97aa0848d2b326675e7bf5
DmalTzi <87565069+DmalTzi@users.noreply.github.com>
2023-04-03 16:40:03 +0700

    ep6

--- Python_ep_06.py
+++ Python_ep_06.py
@@ -10,12 +10,32 @@ class Computer:
         self.ram = ram
 
     def _reportSpecs(self):
+        print("=====================")
         print(f"{self.name} มี SpecsComputer ดังนี้ \nCPU : {self.cpu} \nGPU : {self.gpu} \nMB  : {self.mb} \nSSD : {self.ssd}GB \nPSU : {self.psu} \nRAM : {self.ram}GB")
+        print("=====================")
 
 class Game(Computer):
-    def re4(self):
-        print(self.ram)
+    def _re4remake(self):
+        print(f"คอมสเปคนี้สามารถเล่น RE4remake ได้ไหม")
+        print("เล่นได้") if self.ram >= 8 else print("ไม่สามารถเล่นได้")
+        print("-------------------")
+
+    def _stardew(self):
+        print("คอมสเปคนี้สามารถเล่น Stardew valley ได้ไหม")
+        print("เล่นได้") if self.ram >= 8 else print("ไม่สามารถเล่นได้")
+        print("-------------------")
+
+    def _ow2(self):
+        print("คอมสเปคนี้สามารถเล่น Overwatch2 ได้ไหม")
+        print("เล่นได้") if self.ram >= 8 else print("ไม่สามารถเล่นได้")
+        print("-------------------")
 
 obj1 = Game("แดง","Intel i3-10105f","GTX1050","Auros B356",250,'550W',16)
 obj1._reportSpecs()
-obj1.re4()
+obj1._re4remake()
+obj1._ow2()
+
+obj2 = Game("เขียว", "AMD Ryzen 5 5600", "Radeon rx 570","Auros B550",500,'550W',32)
+obj2._reportSpecs()
+obj2._ow2()
+obj2._stardew()
